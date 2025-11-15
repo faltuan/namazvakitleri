9e949c01f74b8bc7b563192085a2ce7ca6b86334
faltuan <83354250+faltuan@users.noreply.github.com>
2025-11-15 12:03:05 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,20 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("fgold-cache").then((cache) => {
-      return cache.addAll(["/namazvakitleri/index.html", "/namazvakitleri/icon-512.png", "/namazvakitleri/manifest.json"]);
-    })
-  );
+    e.waitUntil(
+        caches.open("fgold-cache").then((cache) => {
+            return cache.addAll([
+                "/namazvakitleri/",
+                "/namazvakitleri/index.html",
+                "/namazvakitleri/icon-512.png",
+                "/namazvakitleri/manifest.json"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
